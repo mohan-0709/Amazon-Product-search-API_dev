4618f8ebf6abab4c67d5b5eb2fa6d0a7654ef752
Mohan Murali K S <79490917+mohan-0709@users.noreply.github.com>
2023-06-27 11:01:39 +0530

    Add files via upload

---
+++ Final_API/new.js
@@ -0,0 +1,149 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const isValidSearchQuery = require('./utils/validation');
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const MAX_RETRIES = 3;
+const RETRY_DELAY = 10000;
+
+let allProducts = [];
+
+// GET endpoint for search
+app.get('/search', async (req, res) => {
+  allProducts = []; // Clear allProducts array
+  const { title, maxPages = 20 } = req.query;
+
+  if (!isValidSearchQuery(title)) {
+    return res.status(400).send('Invalid search query.');
+  }
+
+  let retryCount = 0;
+  let currentPage = 1;
+
+  while (currentPage <= maxPages) {
+    try {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+
+      await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3');
+      await page.goto('https://www.amazon.in/');
+      await page.type('#twotabsearchtextbox', title);
+      await page.click('#nav-search-submit-button');
+      await page.waitForNavigation();
+
+      console.log(`Scraping page ${currentPage}...`);
+
+      const productsOnPage = await page.evaluate(() => {
+        const products = [];
+        const productCards = document.querySelectorAll('.s-result-item');
+        productCards.forEach((card) => {
+          const title = card.querySelector('.a-text-normal');
+          const price = card.querySelector('.a-price-whole');
+          const image = card.querySelector('img');
+          if (title && price && image) {
+            const product = {
+              title: title.innerText,
+              price: parseFloat(price.innerText.replace(/[^0-9.]/g, '')),
+              image: image.getAttribute('src')
+            };
+            products.push(product);
+          }
+        });
+        return products;
+      });
+
+      allProducts = allProducts.concat(productsOnPage);
+
+      await browser.close();
+
+      currentPage++;
+    } catch (error) {
+      console.error(`Error occurred. Retrying (${retryCount}/${MAX_RETRIES})...`);
+      retryCount++;
+      await new Promise(resolve => setTimeout(resolve, RETRY_DELAY));
+    }
+  }
+
+  console.log(`Scraped ${allProducts.length} product(s)`);
+  res.json(allProducts);
+});
+
+// POST endpoint for search
+app.post('/search', async (req, res) => {
+  allProducts = []; // Clear allProducts array
+  const { title, maxPages = 20 } = req.body;
+
+  if (!isValidSearchQuery(title)) {
+    return res.status(400).send('Invalid search query.');
+  }
+
+  let retryCount = 0;
+  let currentPage = 1;
+
+  while (currentPage <= maxPages) {
+    try {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+
+      await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3');
+      await page.goto('https://www.amazon.in/');
+      await page.type('#twotabsearchtextbox', title);
+      await page.click('#nav-search-submit-button');
+      await page.waitForNavigation();
+
+      console.log(`Scraping page ${currentPage}...`);
+
+      const productsOnPage = await page.evaluate(() => {
+        const products = [];
+        const productCards = document.querySelectorAll('.s-result-item');
+        productCards.forEach((card) => {
+          const title = card.querySelector('.a-text-normal');
+          const price = card.querySelector('.a-price-whole');
+          const image = card.querySelector('img');
+          if (title && price && image) {
+            const product = {
+              title: title.innerText,
+              price: parseFloat(price.innerText.replace(/[^0-9.]/g, '')),
+              image: image.getAttribute('src')
+            };
+            products.push(product);
+          }
+        });
+        return products;
+      });
+
+      allProducts = allProducts.concat(productsOnPage);
+
+      await browser.close();
+
+      currentPage++;
+    } catch (error) {
+      console.error(`Error occurred. Retrying (${retryCount}/${MAX_RETRIES})...`);
+      retryCount++;
+      await new Promise(resolve => setTimeout(resolve, RETRY_DELAY));
+    }
+  }
+
+  console.log(`Scraped ${allProducts.length} product(s)`);
+  res.json(allProducts);
+});
+
+// DELETE endpoint to clear search results
+app.delete('/search', (req, res) => {
+  allProducts = []; // Clear allProducts array
+  res.send('Search results cleared.');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}.`);
+});
---
+++ Final_API/readme.md
@@ -0,0 +1,1 @@
+The Final file to be used for documentation.<br> new2.js Done for amazon.com<br>new.js Done for amazon.in
